c63a70976a6ca8d2a795ddc76329376b8d3cf2ae
IlyaM <muntyany.ilya8@gmail.com>
2022-12-28 23:56:15 +0300

    Branch commit

--- MicroserviceTicket/Program.cs
+++ MicroserviceTicket/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MicroserviceCourse
+namespace MicroserviceCourseExample
 {
     public class Program
     {
